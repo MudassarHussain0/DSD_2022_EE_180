095d89843d5d98114f45dff687509fbbc5784754
MudassarHussain0 <mudassarhussain2550@gmail.com>
2025-02-13 09:31:44 +0500

    msg

--- Lab3/test bench.sv
+++ Lab3/test bench.sv
@@ -23,7 +23,7 @@ module lab3_tb;
         a = 0; b = 1; c = 0; #10;
         a = 0; b = 1; c = 1; #10;
         a = 1; b = 0; c = 0; #10;
-        a = 1; b = 0; c = 1; #10;  // This line was fixed by removing the invalid part.
+        a = 1; b = 0; c = 1; #10;  
         a = 1; b = 1; c = 0; #10;
         a = 1; b = 1; c = 1; #10;
         $stop;
